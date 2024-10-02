8898a0e8166d5892673f3dbc538806d3e1c765c9
jasong283 <jasong283@nycstudents.net>
2024-10-02 20:24:19 +0000

    commit

--- src/main/java/com/example/project/Student.java
+++ src/main/java/com/example/project/Student.java
@@ -3,48 +3,64 @@ package com.example.project;
 public class Student {
     // instance variables for 
     // first name (String)
+    private String firstName;
     // last name (String)
+    private String lastName;
     // graduation year (int)
+    private int gradYear;
     // sum of test scores (double).. should initialize at 0.0
+    private double accumulatedTestScores;
     // test score count (int) ..should initialize at 0
+    private int testScoreCount;
     // highest test score (double).. should initialize at 0.0
+    private double highestTestScore;
+    private double averageTestScore;
+
+
  
     // constructor
     public Student(String firstName, String lastName, int gradYear) {
         //implement code here!
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
+        highestTestScore = 0.0;
+        testScoreCount = 0;
+        accumulatedTestScores = 0.0;
 
     }
  
     // returns firstName
     public String getFirstName() {
         //implement code here!
-        return "";
+        return firstName;
     }
  
     // returns lastName
     public String getLastName() {
         //implement code here!
-        return "";
+        return lastName;
     }
  
     public double getHighestTestScore() {
         //implement code here!
-        return 0.0;
+        return highestTestScore;
     }
 
     public int getTestScoreCount(){
         //implement code here!
-        return 0;
+        return testScoreCount;
     }
 
     public int getGradYear(){
         //implement code here!
-        return 0;
+        return gradYear;
     }
  
     // sets gradYear to newGradYear
     public void setGradYear(int newGradYear) {
         //implement code here!
+        gradYear = newGradYear;
     }
  
     // adds newTestScore to accumulatedTestScores
@@ -52,31 +68,38 @@ public class Student {
     // set new highest test score 
     public void addTestScore(double newTestScore) {
         //implement code here!
+        accumulatedTestScores = accumulatedTestScores + newTestScore;
+        testScoreCount++;
+        highestTestScore = newTestScore;
     }
  
     // returns true if the student's average test score is greater
     // than or equal to 65; returns false otherwise (see Note 2 below)
     public boolean isPassing() {
         //implement code here!
-        return false;
+        if((accumulatedTestScores / testScoreCount) >= 65) {
+            return true;
+        }   else {
+            return false;
+        }
     }
  
     // returns the Student's average test score as the
     // quotient of accumulatedTestScores and testScoreCount
     public double averageTestScore() {
         //implement code here!
-        return 0.0;
+        return accumulatedTestScores / testScoreCount;
     }
  
     // this method prints all info of a Student object to the console 
     // I AM NOT TESTING YOU ON THIS METHOD. IT'S FOR YOUR TESTING PURPOSES ONLY.. you don't have to use it
     public void printStudentInfo() {
-        System.out.println("Student Full Name: " );
-        System.out.println("Graduation Year: ");
-        System.out.println("Number of tests: ");
-        System.out.println("Average Test Score: ");
-        System.out.println("Highest Test Score: ");
-        System.out.println("Is passing: ");
+        System.out.println("Student Full Name: " + getFirstName() + " " + getLastName());
+        System.out.println("Graduation Year: " + getGradYear());
+        System.out.println("Number of tests: " + testScoreCount);
+        System.out.println("Average Test Score: " + averageTestScore);
+        System.out.println("Highest Test Score: " + getHighestTestScore());
+        System.out.println("Is passing: " + isPassing());
     }
  }
  
--- src/main/java/com/example/project/StudentRunner.java
+++ src/main/java/com/example/project/StudentRunner.java
@@ -1,5 +1,9 @@
+package com.example.project;
+
 public class StudentRunner{
     public static void main(String[] args) {
         //use this StudentRunner class if you need to test your code 
+        Student stud = new Student("John", "Doe", 2026);
+        stud.printStudentInfo();
     }
 }
